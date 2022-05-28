ffca160872c18d1c3c607318bb4f971c7c73ce44
Ruslan Tretyakov <ruslan.tretyakov2003@gmail.com>
2022-05-28 21:45:15 +0300

    [RRE-12] Implemented Repository pattern

---
+++ DAL/Interfaces/IDataRepository.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq.Expressions;
+
+namespace DAL.Interfaces
+{
+    public interface IDataRepository<T> where T : class
+    {
+        Task<T> GetAsync(int id);
+
+        Task<IEnumerable<T>> GetAllAsync();
+
+        Task AddAsync(T entity);
+
+        Task AddRangeAsync(IEnumerable<T> entities);
+
+        Task<T> FindAsync(Expression<Func<T, bool>> expression);
+
+        Task<IEnumerable<T>> FindRangeAsync(Expression<Func<T, bool>> expression);
+
+        void Update(T entity);
+
+        void Remove(T entity);
+
+        void RemoveRange(IEnumerable<T> entities);
+        bool NotEmpty(int id);
+        bool Select(int id);
+    }
+}
