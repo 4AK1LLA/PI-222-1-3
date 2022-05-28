046856bc2bd4beb5a366d30a4b4878f94c36c97e
Ruslan Tretyakov <ruslan.tretyakov2003@gmail.com>
2022-05-28 22:22:23 +0300

    [RRE-13] Implemented UnitOfWork

---
+++ DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using DAL.Models;
+
+namespace DAL.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IDataRepository<BodyType> BodyTypeRepository { get; }
+        IDataRepository<Car> CarRepository { get; }
+        IDataRepository<CarsListViewModel> CarsListViewModelRepository { get; }
+        IDataRepository<Color> ColorRepository { get; }
+        IDataRepository<Customer> CustomerRepository { get; }
+        IDataRepository<Drive> DriveRepository { get; }
+        IDataRepository<Manager> ManagerRepository { get; }
+        IDataRepository<Model> ModelRepository { get; }
+        IDataRepository<Order> OrderRepository { get; }
+        IDataRepository<OrderType> OrderTypeRepository { get; }
+        IDataRepository<Service> ServiceRepository { get; }
+        IDataRepository<User> UserRepository { get; }
+
+        Task<bool> ConfirmAsync();
+    }
+}
---
+++ DAL/UnitOfWork.cs
@@ -0,0 +1,47 @@
+﻿using DAL.Models;
+using DAL.Contexts;
+using DAL.Interfaces;
+
+namespace DAL
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AutoShowContext _context;
+        public IDataRepository<BodyType> BodyTypeRepository { get; private set; }
+        public IDataRepository<Car> CarRepository { get; private set; }
+        public IDataRepository<CarsListViewModel> CarsListViewModelRepository { get; private set; }
+        public IDataRepository<Color> ColorRepository { get; private set; }
+        public IDataRepository<Customer> CustomerRepository { get; private set; }
+        public IDataRepository<Drive> DriveRepository { get; private set; }
+        public IDataRepository<Manager> ManagerRepository { get; private set; }
+        public IDataRepository<Model> ModelRepository { get; private set; }
+        public IDataRepository<Order> OrderRepository { get; private set; }
+        public IDataRepository<OrderType> OrderTypeRepository { get; private set; }
+        public IDataRepository<Service> ServiceRepository { get; private set; }
+        public IDataRepository<User> UserRepository { get; private set; }
+
+        public UnitOfWork(AutoShowContext context)
+        {
+            _context = context;
+            _context.Database.EnsureCreated();
+            
+            BodyTypeRepository = new DataRepository<BodyType>(context);
+            CarRepository = new DataRepository<Car>(context);
+            CarsListViewModelRepository = new DataRepository<CarsListViewModel>(context);
+            ColorRepository = new DataRepository<Color>(context);
+            CustomerRepository = new DataRepository<Customer>(context);
+            DriveRepository = new DataRepository<Drive>(context);
+            ManagerRepository = new DataRepository<Manager>(context);
+            ModelRepository = new DataRepository<Model>(context);
+            OrderRepository = new DataRepository<Order>(context);
+            OrderTypeRepository = new DataRepository<OrderType>(context);
+            ServiceRepository = new DataRepository<Service>(context);
+            UserRepository = new DataRepository<User>(context);
+        }
+
+        public async Task<bool> ConfirmAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
